952e51334dd07bc318d36ea08e5a5cfe3c7dddb7
shivvkumar07 <shivk67008@gmail.com>
2026-02-09 07:57:15 +0000

    01_basics

---
+++ 01_basics/03_type_conversion.js
@@ -0,0 +1,13 @@
+let score ="33"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber)
+
+
+let isloggedIn = 1;
+
+let booleanIslogedIn = Boolean(isloggedIn);
+console.log(booleanIslogedIn);
