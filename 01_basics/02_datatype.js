7ee2cdd71724c1cbaa0fe9cd2da8d0c6bd9d2975
shivvkumar07 <shivk67008@gmail.com>
2026-02-09 07:07:55 +0000

    01_basics

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,15 @@
+"use strict" //treat all js as a new code 
+
+//alert("hello");  we are using node js not browser
+
+
+
+
+console.log(3+3)
+console.log("shiv")
+
+let name ="shiv"
+let age = 18;
+let loggedIn = true;
+
+console.log(typeof age);
