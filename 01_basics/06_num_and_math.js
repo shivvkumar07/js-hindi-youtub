628049ee8bb9e82da2733a2017a89ef62228039c
shivvkumar07 <shivk67008@gmail.com>
2026-03-10 13:57:28 +0000

    math

--- 01_basics/06_num_and_math.js
+++ 01_basics/06_num_and_math.js
@@ -8,3 +8,15 @@ console.log(balance);
 
 
 console.log(balance.toString());
+
+console.log(Math);
+
+console.log(Math.abs(-4));
+console.log(Math.round(4.33));
+console.log(Math.round(4.66));
+console.log(Math.ceil(5.2));
+console.log(Math.floor(5.2));
+console.log(Math.min(1,2,5,6));
+console.log(Math.max(4,6,7,8,22));
+
+console.log(Math.random());
