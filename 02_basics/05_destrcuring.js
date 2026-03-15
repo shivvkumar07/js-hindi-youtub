c7c83708b7a584f064215166897db9a57d2fa2c8
shivvkumar07 <shivk67008@gmail.com>
2026-03-15 04:02:47 +0000

    api

---
+++ 02_basics/05_destrcuring.js
@@ -0,0 +1,26 @@
+const course = {
+    coursename:"js in hindi",
+    price:"999",
+    courseInstructor:"hitesh"
+}
+
+
+// console.log(course.courseInstructor);
+
+const {courseInstructor}= course;
+
+console.log(courseInstructor);
+
+
+// const navbar = ({}) => {
+
+// }
+// api ka syntax
+// {
+//     "name":"shiv",
+//     "course":"js in hindi",
+//     "price":"free"
+// }
+
+// note :kabhi kabhi api array ke form mein milti h 
+
