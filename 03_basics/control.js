4edfc8c573f2beac1c4678d2540f6628e149b8df
shivvkumar07 <shivk67008@gmail.com>
2026-03-19 06:15:41 +0000

    if else

---
+++ 03_basics/control.js
@@ -0,0 +1,21 @@
+// const score = 200;
+
+
+// if(score >100){
+//     const power = "fly";
+//     console.log(`user power : ${power}`);
+// }
+
+
+// const balance = 1000;
+
+
+// if(balance>500) console.log("test");
+
+
+const userLoggedIn = true;
+const debitCard = true;
+
+if(userLoggedIn && debitCard){
+    console.log("allow to buy the courses");
+}
